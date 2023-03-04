498f3397c0e00828ca7483d7e0f2a70cf515a4ac
arnotoro <arno.toro@gmail.com>
2023-03-04 13:18:19 +0200

    register form front end

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,16 +1,58 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Form, Button, Row, Col} from 'react-bootstrap'
 import '../assets/styles/register.css'
 
+
+const initialState = {
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+};
+
+const passwordValidator = {
+    isLenthy: false,
+    hasNumber: false,
+    hasSpecialChar: false,
+    hasUpperCase: false,
+    hasLowerCase: false,
+    confirmPassword: false,
+};
+
 const Register = () => {
-
+    // validate form
     const [validated, setValidated] = useState(false);
+    const [formData, setFormData] = useState(initialState);
+    const [passwordError, setPasswordError] = useState(passwordValidator);
 
+    useEffect(() => {
+
+    }, [formData]);
+
+    const handleOnChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+
+        if (name === 'password') {
+            const isLenthy = value.length >= 8;
+            const hasNumber = /[0-9]/.test(value);
+            const hasSpecialChar = /[!,@,#,$,%,^,&,*]/.test(value);
+            const hasUpperCase = /[A-Z]/.test(value);
+            const hasLowerCase = /[a-z]/.test(value);
+
+            setPasswordError({...passwordError, isLenthy, hasNumber, hasSpecialChar, hasUpperCase, hasLowerCase});
+        } 
+        if (name === 'confirmPassword') {
+            setPasswordError({...passwordError, confirmPassword: value === formData.password})
+        }
+
+    };
     const handleSubmit = (event) => {
         const form = event.target;
         const data = new FormData(form);
-        
 
         if (form.checkValidity() === false) {
             event.preventDefault();
@@ -21,6 +63,7 @@ const Register = () => {
         if (form.checkValidity() === true) {
             // send data to backend
             console.log(data);
+            console.log('hello world')
 
         }
     };
@@ -28,16 +71,20 @@ const Register = () => {
     return (
         <div className="register-container">
             <Form noValidate validated={validated} onSubmit={handleSubmit} className="register-form">
-                <h2 style={{textAlign: 'center'}}> Register to CodeSnipper</h2>
+                <h2 className="ms-1"> Register to CodeSnipper</h2>
+                <Form.Text className="text-muted ms-1"> Please fill out all the fields below to register. </Form.Text>
                 <Row className='mb-2 mt-1 ms-0 me-0'>
 
                     {/* First name input field */}
-                    <Form.Group as={Col} controlid="formGridFirstName">
+                    <Form.Group as={Col}>
                         <Form.Label>First Name</Form.Label>
                         <Form.Control
                             className='ms-0 me-0'
                             type="text"
+                            name="firstName"
+                            value={formData.firstName}
                             placeholder="First name"
+                            onChange={handleOnChange}
                             required
                         />
                         <Form.Control.Feedback type="invalid">
@@ -46,12 +93,15 @@ const Register = () => {
                     </Form.Group>
 
                     {/* Last name input field */}
-                    <Form.Group as={Col} controlid="formGridLastName">
+                    <Form.Group as={Col}>
                         <Form.Label>Last Name</Form.Label>
                         <Form.Control
                             className='ms-0 me-0'
                             type="text"
+                            name="lastName"
+                            value={formData.lastName}
                             placeholder="Last name"
+                            onChange={handleOnChange}
                             required
                         />
                         <Form.Control.Feedback type="invalid">
@@ -60,12 +110,15 @@ const Register = () => {
                     </Form.Group>
 
                     {/* Username input field */}
-                    <Form.Group as={Col} controlid="formGridUsername">
+                    <Form.Group as={Col}>
                         <Form.Label>Username</Form.Label>
                         <Form.Control
                             className='ms-0 me-0'
                             type="text"
+                            name="username"
+                            value={formData.username}
                             placeholder="Username"
+                            onChange={handleOnChange}
                             required
                         />
                         <Form.Control.Feedback type="invalid">
@@ -77,12 +130,15 @@ const Register = () => {
 
                 <Row className='mb-2 mt-1 ms-0 me-0'>
                     {/* Email input field */}
-                    <Form.Group as={Col} controlid="formGridEmail">
+                    <Form.Group as={Col}>
                         <Form.Label>Email</Form.Label>
                         <Form.Control
                             className='ms-0 me-0'
                             type="email"
+                            name="email"
+                            value={formData.email}
                             placeholder="Enter email"
+                            onChange={handleOnChange}
                             required
                         />
                         <Form.Control.Feedback type="invalid">
@@ -92,22 +148,55 @@ const Register = () => {
                 </Row>
 
                 <Row className='mb-2 mt-1 ms-0 me-0'>
-                    {/* Password input field */}
-                    <Form.Group as={Col} controlid="formGridPassword">
+                    {/* Password1 input field */}
+                    <Form.Group as={Col}>
                         <Form.Label>Password</Form.Label>
                         <Form.Control 
-                            className='mb-2 me-1' 
-                            type="password" 
+                            className='mb-1 me-1' 
+                            type="password"
+                            name="password"
+                            value={formData.password}
                             placeholder="Enter password"
+                            onChange={handleOnChange}
                             required
                         />
                         <Form.Control.Feedback type="invalid">
                             Please provide a password.
                         </Form.Control.Feedback>
                     </Form.Group>
+
+                    {/* Password2 input field */}
+                    <Form.Group as={Col}>
+                        <Form.Label>Confirm password</Form.Label>
+                        <Form.Control 
+                            className='mb-1 me-1' 
+                            type="password"
+                            name="confirmPassword"
+                            value={formData.confirmPassword}
+                            placeholder="Enter password again"
+                            onChange={handleOnChange}
+                            required
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a password.
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Text className='mb-0'>
+                        <span>Password requirements:</span>
+                        <ul>
+                            <li className={passwordError.isLenthy ? "text-successs" : "text-danger"}>At least 8 characters</li>
+                            <li className={passwordError.hasUpperCase ? "text-successs" : "text-danger"}>At least one upper case</li>
+                            <li className={passwordError.hasLowerCase ? "text-successs" : "text-danger"}>At least one lower case</li>
+                            <li className={passwordError.hasNumber ? "text-successs" : "text-danger"}>At least one number</li>
+                            <li className={passwordError.hasSpecialChar ? "text-successs" : "text-danger"}>At least one special character</li>
+                            <li className={passwordError.confirmPassword ? "text-successs" : "text-danger"}>Passwords need to match</li>
+                        </ul>
+                    </Form.Text>
                 </Row>
+
+
                 <Row className='mb-2 mt-1 ms-1 me-1'>
-                    <Button type="submit" variant='primary' className="mb-2 me-2">Register</Button>
+                    <Button type="submit" variant='primary' className="mb-2 me-2" disabled={Object.values(passwordError).includes(false)}>Register</Button>
                 </Row >
             </Form>
         </div>
